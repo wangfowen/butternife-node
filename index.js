557a68c7d2c0e6f867240f8b316a5322d3a074de
Kristopher Windsor <kwindsor@axcient.com>
2013-03-03 02:39:20 -0800

    catch 400 error

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
-/*
-// hi
-console.log('your mom');
-
-var port = process.env.PORT || 8000;
-
-// Load the http module to create an http server.
-var http = require('http');
-
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World\n");
-});
-
-// Listen on port, IP defaults to 127.0.0.1
-server.listen(port);
-
-// Put a friendly message on the terminal
-console.log("Server running at http://127.0.0.1:" + port + "/");
-*/
 
 
 
@@ -26,16 +5,19 @@ console.log("Server running at http://127.0.0.1:" + port + "/");
 
 
 
-var express = require('express');//,
+
+var express = require('express');
 ///wines = require('./routes/wines');
 var app = express.createServer();
 var port = process.env.PORT || 8000;
 
 app.configure(function(){
   app.use(express.bodyParser());
+  app.use(function(err,req,res,next){
+    res.send(400, '');
+  });
 });
 
-//app.get('/', wines.findAll);
 app.get('/trip/:id', function (req, res) {});
 
 app.post('/routing', function (req, res) {
