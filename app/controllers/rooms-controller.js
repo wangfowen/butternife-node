8cb92021a590f560e450180b0fcb8d6d43cf7ab9
Owen Wang <wangfowen@gmail.com>
2013-04-10 22:49:05 -0700

    it worksgs

--- app/controllers/rooms-controller.js
+++ app/controllers/rooms-controller.js
@@ -4,13 +4,8 @@ module.exports = function(app, io) {
 		var params = {
 			name: req.params.roomName
 		};
-		//TODO: is this how you render?
 		res.render('show.ejs', params);
 	});
-
-	// app.get('/butternife', function(req, res) {
-	// 	//TODO: render new.ejs
-	// });
 
 	app.get('/', function(req, res) {
 		res.render('new.ejs', {});
--- public/js/new.js
+++ public/js/new.js
@@ -1,43 +1,45 @@
-$('#party-tooltip').tooltip({
-  title: "One person plays the music, everyone adds songs"
-});
+$(function() {
+  $('#party-tooltip').tooltip({
+    title: "One person plays the music, everyone adds songs"
+  });
 
-$('#shared-tooltip').tooltip({
-  title: "Everyone plays and adds songs"
-});
+  $('#shared-tooltip').tooltip({
+    title: "Everyone plays and adds songs"
+  });
 
-var user;
+  var user;
 
-/*
-$.ajax({
-  dataType:'jsonp',
-  jsonp:'callback',
-  url:'http://192.168.31.24:8000/user?callback=?',
-  success: function(data) {
-    user = data;
-    console.log("user: " + user);
-  }
-});
-*/
-
-$('#join-submit').click(function(e) {
-  e.stopPropagation();
-  e.preventDefault();
-
-  var joinName = $('#join-name').val(),
-    mode = $('input[name=music-mode]:checked').val();
-
-/*
+  /*
   $.ajax({
     dataType:'jsonp',
     jsonp:'callback',
-    data: {user: user.id, mode: mode},
-    url:'http://192.168.31.24:8000/butternife/' + joinName + '/user?callback=?',
+    url:'http://192.168.31.24:8000/user?callback=?',
     success: function(data) {
-      //store data in cookie
-      window.location.href = "/butternife/" + joinName;
+      user = data;
+      console.log("user: " + user);
     }
   });
-*/
-  window.location.href = "/butternife/" + joinName;
+  */
+
+  $('#join-submit').click(function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var joinName = $('#join-name').val(),
+      mode = $('input[name=music-mode]:checked').val();
+
+  /*
+    $.ajax({
+      dataType:'jsonp',
+      jsonp:'callback',
+      data: {user: user.id, mode: mode},
+      url:'http://192.168.31.24:8000/butternife/' + joinName + '/user?callback=?',
+      success: function(data) {
+        //store data in cookie
+        window.location.href = "/butternife/" + joinName;
+      }
+    });
+  */
+    window.location.href = "/" + joinName;
+  });
 });
